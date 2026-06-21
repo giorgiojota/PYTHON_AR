ba4d3e85339e4c0ba04b39036e659f9a8766f124
Giorgio Jota <giorgiojota@gmail.com>
2026-06-20 20:18:10 -0400

    Hola, este es mi primer script del curso AR

---
+++ PYTHON_AR.py
@@ -0,0 +1,7 @@
+# Curso Python AR
+# Mi progreso en el curso de Python - Academia AR
+# Fecha: 21 de junio de 2026
+
+print("Hola, este es mi primer script del curso AR")
+print("Aquí iré registrando mis avances")
+
